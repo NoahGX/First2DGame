1126db1e19f396333182763e66622f3bc30035eb
NoahGX <114901822+NoahGX@users.noreply.github.com>
2022-10-26 16:19:41 -0700

    Change Size After Repeat

--- First2DGame/Assets/MovingBlock.cs
+++ First2DGame/Assets/MovingBlock.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class MovingBlock : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 3f;
+    float startingYPosition;
+
+    void Start()
+    {
+        startingYPosition = transform.position.y;
+    }
 
     void Update()
     {
@@ -12,6 +18,8 @@ public class MovingBlock : MonoBehaviour
         if (transform.position.x <= -15)
         {
             transform.position += Vector3.right * 30f;
+            float newY = startingYPosition + UnityEngine.Random.Range(-1f, 1f);
+            transform.position = new Vector3(transform.position.x, newY, 0f);
         }
     }
 }
