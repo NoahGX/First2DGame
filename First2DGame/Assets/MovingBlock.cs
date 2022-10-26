072c48df4491015e3b0f61c74d86dfcd0ee21b7f
NoahGX <114901822+NoahGX@users.noreply.github.com>
2022-10-26 16:01:21 -0700

    Repeat Scene

--- First2DGame/Assets/MovingBlock.cs
+++ First2DGame/Assets/MovingBlock.cs
@@ -9,5 +9,9 @@ public class MovingBlock : MonoBehaviour
     void Update()
     {
         transform.position += Vector3.left * Time.deltaTime * moveSpeed;
+        if (transform.position.x <= -15)
+        {
+            transform.position += Vector3.right * 30f;
+        }
     }
 }
